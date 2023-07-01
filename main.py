ab938e3cb40fe362516e12cb7f15c6fd550e16bc
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-07-01 15:22:13 +0530

    updated code
    Modified TICKER, START_DATE, END_DATE

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 from yfin.transformers.yfin_transformer import YfinETL
 
 if __name__ == "__main__":
-    TICKER = ['AAPL']
+    TICKER = 'AMZN'
     START_DATE = '2020-01-01'
-    END_DATE = '2020-02-28'
+    END_DATE = '2023-01-31'
     data = YfinETL(TICKER, START_DATE, END_DATE)
     data.etl_report()
