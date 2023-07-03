ad27db207a84c8c18fb02f39aa184058480083fc
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-07-03 21:57:58 +0530

    extracting data for AAP ticker

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from yfin.transformers.yfin_transformer import YfinETL
 
 if __name__ == "__main__":
-    TICKER = 'TSLA'
+    TICKER = 'AAP'
     START_DATE = '2020-01-01'
     END_DATE = '2023-01-31'
     data = YfinETL(TICKER, START_DATE, END_DATE)
