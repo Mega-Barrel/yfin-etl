d3b33f736ccba8f82d612e0709e00f9d9dfa8ee8
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-07-04 22:18:03 +0530

    modified Ticker, Start, end date

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 from yfin.transformers.yfin_transformer import YfinETL
 
 if __name__ == "__main__":
-    TICKER = 'AAP'
-    START_DATE = '2020-01-01'
-    END_DATE = '2023-01-31'
+    TICKER = 'AAPL'
+    START_DATE = '2023-01-01'
+    END_DATE = '2023-06-07'
     data = YfinETL(TICKER, START_DATE, END_DATE)
     data.etl_report()
