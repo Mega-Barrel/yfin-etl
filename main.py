ec156244ecb1e430e713035976fedeece8278b24
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-07-05 21:51:32 +0530

    Added time_it decorator to keep track of time the code took to execute

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 """Main file for Yfin ETL"""
 
+from yfin.common.yfin_logger import logger
+from yfin.common.time_logger import time_it
 from yfin.transformers.yfin_transformer import YfinETL
 
+@time_it
+def main():
+    """
+    Main function
+    """
+    try:
+        logger.info('Process Started for function: main ')
+
+        # Input Params
+        ticker = 'AAPL'
+        start_date = '2020-01-01'
+        end_date = '2023-05-31'
+
+        # aCreating YfinETL object
+        data = YfinETL(ticker, start_date=start_date, end_date=end_date)
+        data.etl_report()
+        logger.info('Process Completed for funtion: main')
+    except Exception as error: #pylint: disable=W0718
+        logger.error('Error occured in function: etl_report')
+        logger.error(error)
+
 if __name__ == "__main__":
-    TICKER = 'AAPL'
-    START_DATE = '2023-01-01'
-    END_DATE = '2023-06-07'
-    data = YfinETL(TICKER, START_DATE, END_DATE)
-    data.etl_report()
+    main()
